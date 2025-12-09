8b2101762294b80abe7f44a11649a41ac80a39c0
Collins Kibet <kollcibe05@gmail.com>
2025-12-09 19:27:48 +0300

    testing both methods

--- lib/tree.py
+++ lib/tree.py
@@ -9,9 +9,9 @@ class Tree:
         node = nodes_to_visit.pop(0)
         if node["id"] == id:
             result.append(node)
-            return node
+            return node          ##Where the answer is from 
         else:
-            nodes_to_visit = node["children"] + nodes_to_visit 
+            nodes_to_visit = node["children"]  + nodes_to_visit             #nodes_to_visit = nodes_to_visit  + node["children"]
     return result or None          
     pass
 
